060fbd58a96dcb648dc8cb03e0e581c29a4bb4ee
Henry Twist <henrytwistprojects@gmail.com>
2026-05-05 01:14:09 +0100

    Site concept

---
+++ astro.config.mjs
@@ -0,0 +1,16 @@
+// @ts-check
+import { defineConfig, fontProviders } from 'astro/config';
+
+import tailwindcss from '@tailwindcss/vite';
+
+// https://astro.build/config
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()]
+  },
+  fonts: [{
+	provider: fontProviders.google(),
+	name: "Lato",
+	cssVariable: "--font-default"
+  }]
+});
